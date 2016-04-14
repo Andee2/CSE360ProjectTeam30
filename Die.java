a3423ca61d09f640728d8de8a9800d07045d0f53
Andee <andeel@cox.net>
2016-04-13 18:40:59 -0700

    Added files via upload

---
+++ Die.java
@@ -0,0 +1,23 @@
+/**
+ * @author Andrew Leach
+ * This class initializes a random number generator between 1 and 6
+ * to act as the rolling of a 6 sided die.
+ */
+
+import java.util.Random;
+
+public class Die
+{
+	Random rand;
+
+	public Die()
+	{
+		rand = new Random();
+	}
+
+	public int roll()
+	{
+		return rand.nextInt(6) + 1;
+	}
+
+}
