181d8a8d1bcc6ce02ab93ae4519c76d2e5ba17ed
Andee <andeel@cox.net>
2016-04-15 14:59:52 -0700

    Added Comments and fixed logic
    Fixed incorrect logic on mRoll. The method was selecting from 1 to 12 inclusive, and now it is selecting from 2 to 12 inclusive.

--- Die.java
+++ Die.java
@@ -8,21 +8,32 @@ import java.util.Random;
 
 public class Die
 {
-	private Random rand;
+	private Random rand; //Uses java.util.Random to simulate a dice roll
 
+	/**
+	 * Constructor for Die.class
+	 */
 	public Die()
 	{
 		rand = new Random();
 	}
 
+	/**
+	 * Simulates the roll of a six sided die
+	 * @return Integer value between 1 and 6
+	 */
 	public int roll()
 	{
 		return rand.nextInt(6) + 1;
 	}
-	
+
+	/**
+	 * Reuses the Random object already initialized to simulate the monster's guess
+	 * @return Integer value between 2 and 12
+	 */
 	public int mRoll()
 	{
-		return rand.nextInt(12) + 1;
+		return rand.nextInt(11) + 2;
 	}
 
 }
