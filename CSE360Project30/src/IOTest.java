704134c5ebcc541d0fc1b4d0404a3cb2a2f0bd68
Marc Chun <mkchun@asu.edu>
2016-04-15 14:26:46 -0700

    Added in IOTest

---
+++ CSE360Project30/src/IOTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StreamCorruptedException;
+import org.junit.Test;
+
+public class IOTest 
+{
+
+	@Test
+	public void testWrite() 
+			throws FileNotFoundException, NullPointerException, IOException 
+	{
+		String username = "Aidan";
+		Player myPlayer = new Player(username);
+		int error = IO.write(myPlayer, username);
+
+		assertEquals(error, 0);
+	}
+	
+	@Test
+	public void testRetrieve() 
+			throws StreamCorruptedException, FileNotFoundException, NullPointerException, IOException 
+	{
+		String username = "Aidan";
+		Player myPlayer = IO.retrieve(username);
+		
+		assertNotEquals(myPlayer, null);
+	}
+
+}
